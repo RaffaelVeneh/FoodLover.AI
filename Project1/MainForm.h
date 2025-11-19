0fe52aa141583ba0852a93ecbc71aeaf815c6659
zidan-cmiw <b3nny.haryant0@gmail.com>
2025-11-19 12:16:28 +0700

    nambah data

--- Project1/MainForm.h
+++ Project1/MainForm.h
@@ -150,6 +150,10 @@ namespace FoodLover {
 	private: List<FoodLover::Menu^>^ databaseMenu;
 	private: System::Windows::Forms::Button^ btnFeedback;
 
+
+private: System::Windows::Forms::Button^ button3;
+private: System::Windows::Forms::Label^ label1;
+
 	protected:
 
 	private:
@@ -172,32 +176,37 @@ namespace FoodLover {
 			this->labelBahan = (gcnew System::Windows::Forms::Label());
 			this->txtBahan = (gcnew System::Windows::Forms::TextBox());
 			this->btnFeedback = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// labelRasa
 			// 
 			this->labelRasa->AutoSize = true;
-			this->labelRasa->Location = System::Drawing::Point(12, 218);
+			this->labelRasa->Location = System::Drawing::Point(18, 335);
+			this->labelRasa->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->labelRasa->Name = L"labelRasa";
-			this->labelRasa->Size = System::Drawing::Size(85, 13);
+			this->labelRasa->Size = System::Drawing::Size(285, 20);
 			this->labelRasa->TabIndex = 0;
-			this->labelRasa->Text = L"Preferensi Rasa:";
+			this->labelRasa->Text = L"Preferensi Rasa/Rasa yang Diinginkan:";
 			this->labelRasa->Click += gcnew System::EventHandler(this, &MainForm::label1_Click);
 			// 
 			// comboRasa
 			// 
 			this->comboRasa->FormattingEnabled = true;
 			this->comboRasa->Items->AddRange(gcnew cli::array< System::Object^  >(4) { L"Semua", L"Pedas", L"Manis", L"Gurih" });
-			this->comboRasa->Location = System::Drawing::Point(15, 234);
+			this->comboRasa->Location = System::Drawing::Point(22, 369);
+			this->comboRasa->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->comboRasa->Name = L"comboRasa";
-			this->comboRasa->Size = System::Drawing::Size(121, 21);
+			this->comboRasa->Size = System::Drawing::Size(180, 28);
 			this->comboRasa->TabIndex = 1;
 			// 
 			// btnCari
 			// 
-			this->btnCari->Location = System::Drawing::Point(151, 232);
+			this->btnCari->Location = System::Drawing::Point(226, 366);
+			this->btnCari->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->btnCari->Name = L"btnCari";
-			this->btnCari->Size = System::Drawing::Size(108, 23);
+			this->btnCari->Size = System::Drawing::Size(162, 35);
 			this->btnCari->TabIndex = 2;
 			this->btnCari->Text = L"Cari Rekomendasi";
 			this->btnCari->UseVisualStyleBackColor = true;
@@ -206,43 +215,70 @@ namespace FoodLover {
 			// listHasil
 			// 
 			this->listHasil->FormattingEnabled = true;
-			this->listHasil->Location = System::Drawing::Point(15, 261);
+			this->listHasil->ItemHeight = 20;
+			this->listHasil->Location = System::Drawing::Point(22, 449);
+			this->listHasil->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->listHasil->Name = L"listHasil";
-			this->listHasil->Size = System::Drawing::Size(472, 147);
+			this->listHasil->Size = System::Drawing::Size(671, 224);
 			this->listHasil->TabIndex = 3;
 			// 
 			// labelBahan
 			// 
 			this->labelBahan->AutoSize = true;
-			this->labelBahan->Location = System::Drawing::Point(12, 9);
+			this->labelBahan->Location = System::Drawing::Point(18, 14);
+			this->labelBahan->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->labelBahan->Name = L"labelBahan";
-			this->labelBahan->Size = System::Drawing::Size(126, 13);
+			this->labelBahan->Size = System::Drawing::Size(284, 20);
 			this->labelBahan->TabIndex = 4;
-			this->labelBahan->Text = L"Masukkan bahan-bahan:";
+			this->labelBahan->Text = L"Masukkan bahan-bahan yang tersedia:";
 			// 
 			// txtBahan
 			// 
-			this->txtBahan->Location = System::Drawing::Point(15, 25);
+			this->txtBahan->Location = System::Drawing::Point(22, 38);
+			this->txtBahan->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->txtBahan->Multiline = true;
 			this->txtBahan->Name = L"txtBahan";
-			this->txtBahan->Size = System::Drawing::Size(472, 178);
+			this->txtBahan->Size = System::Drawing::Size(671, 272);
 			this->txtBahan->TabIndex = 5;
 			// 
 			// btnFeedback
 			// 
-			this->btnFeedback->Location = System::Drawing::Point(15, 414);
+			this->btnFeedback->Location = System::Drawing::Point(22, 684);
+			this->btnFeedback->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->btnFeedback->Name = L"btnFeedback";
-			this->btnFeedback->Size = System::Drawing::Size(141, 29);
+			this->btnFeedback->Size = System::Drawing::Size(193, 53);
 			this->btnFeedback->TabIndex = 6;
 			this->btnFeedback->Text = L"Menu Tidak Ditemukan\?";
 			this->btnFeedback->UseVisualStyleBackColor = true;
 			this->btnFeedback->Click += gcnew System::EventHandler(this, &MainForm::btnFeedback_Click);
 			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(226, 684);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(162, 53);
+			this->button3->TabIndex = 9;
+			this->button3->Text = L"Selesai";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MainForm::button3_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(18, 424);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(221, 20);
+			this->label1->TabIndex = 10;
+			this->label1->Text = L"Rekomendasi Menu Makanan";
+			// 
 			// MainForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(499, 503);
+			this->ClientSize = System::Drawing::Size(706, 767);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button3);
 			this->Controls->Add(this->btnFeedback);
 			this->Controls->Add(this->txtBahan);
 			this->Controls->Add(this->labelBahan);
@@ -250,6 +286,7 @@ namespace FoodLover {
 			this->Controls->Add(this->btnCari);
 			this->Controls->Add(this->comboRasa);
 			this->Controls->Add(this->labelRasa);
+			this->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->Name = L"MainForm";
 			this->Text = L"MainForm";
 			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
@@ -357,5 +394,8 @@ namespace FoodLover {
 
 		form->ShowDialog();
 	}
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->Close();
+}
 };
 }
